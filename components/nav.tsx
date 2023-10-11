3e5a47ac352f81d35fdd11bf457d260f5b2ca38a
Piotr Godzień <piotrgodzien@gmail.com>
2023-10-11 22:16:03 +0200

    Update nav.tsx

--- components/nav.tsx
+++ components/nav.tsx
@@ -90,6 +90,7 @@ export default function Example() {
                             <div className="py-6">
                                 <a
                                     href="#kontakt"
+                                    onClick={() => setMobileMenuOpen(false)}
                                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                                 >
                                     Skontaktuj się
