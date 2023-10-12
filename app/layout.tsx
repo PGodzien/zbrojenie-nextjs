4e96634119d95f11df110abe8a4efaad0445e0b7
Piotr Godzień <piotrgodzien@gmail.com>
2023-10-12 13:41:51 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import './globals.css'
 import { Space_Grotesk } from 'next/font/google'
 import Footer from '@/components/footer'
 import Contact from '@/components/contact'
+import Script from 'next/script'
+
+const GTM_ID = 'GTM-NVXXW9C';
 
 const inter = Space_Grotesk({
   weight: ["300", "400", "500", "600", "700",],
@@ -16,11 +19,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pl">
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
       <body className={inter.className}>
         <Nav />
         {children}
         <Contact />
         <Footer />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+          }}
+        />
       </body>
     </html>
   )
