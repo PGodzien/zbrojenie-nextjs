7ea9df1324f05e0ee9984dea9fa4e28c79eaa693
Piotr Godzień <piotrgodzien@gmail.com>
2023-10-19 14:29:23 +0200

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -1,7 +1,20 @@
+'use client'
 import Image from "next/image"
 import slider from '@/public/slider_02.jpg'
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment, useState } from 'react'
+import { PlayCircleIcon, XCircleIcon } from "@heroicons/react/24/outline"
 
 export default function About() {
+    let [isOpen, setIsOpen] = useState(false)
+
+    function closeModal() {
+        setIsOpen(false)
+    }
+
+    function openModal() {
+        setIsOpen(true)
+    }
     return (
         <div className="border-b bg-gray-50">
             <div className="relative max-w-7xl mx-auto overflow-hidden border-x">
@@ -11,6 +24,61 @@ export default function About() {
                         src={slider}
                         alt=""
                     />
+                    <div className="absolute inset-0 z-10 flex justify-center items-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                        <button
+                            type="button"
+                            onClick={openModal}
+                            className="w-40 h-40 text-white hover:text-orange-500 duration-200 transition"
+                        >
+                            <PlayCircleIcon />
+                        </button>
+                    </div>
+
+                    <Transition appear show={isOpen} as={Fragment}>
+                        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0"
+                                enterTo="opacity-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100"
+                                leaveTo="opacity-0"
+                            >
+                                <div className="fixed inset-0 bg-black bg-opacity-90" />
+
+                            </Transition.Child>
+
+                            <div className="fixed inset-0 overflow-y-auto">
+                                <div className="flex min-h-full items-center justify-center p-4 text-center">
+                                    <Transition.Child
+                                        as={Fragment}
+                                        enter="ease-out duration-300"
+                                        enterFrom="opacity-0 scale-95"
+                                        enterTo="opacity-100 scale-100"
+                                        leave="ease-in duration-200"
+                                        leaveFrom="opacity-100 scale-100"
+                                        leaveTo="opacity-0 scale-95"
+                                    >
+                                        <Dialog.Panel className="w-full max-w-4xl mx-auto shadow-xl transition-all">
+                                            <button
+                                                type="button"
+                                                className="absolute text-gray-400 hover:text-white z-50 w-20 h-20 -top-20 right-0"
+                                                onClick={closeModal}
+                                            >
+                                                <XCircleIcon />
+                                            </button>
+                                            <div className="">
+                                                <div className="w-full h-full">
+                                                    <iframe width="100%" height="450" src="https://www.youtube.com/embed/mVuWdZlw8eg?si=uqtcUIXOfNWtj8wm" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                                                </div>
+                                            </div>
+                                        </Dialog.Panel>
+                                    </Transition.Child>
+                                </div>
+                            </div>
+                        </Dialog>
+                    </Transition>
                     <div className="absolute inset-0 bg-gradient-to-br from-black via-black to-orange-700 opacity-50"></div>
                 </div>
                 <div className="relative mx-auto max-w-7xl py-24 sm:py-32 lg:px-8 lg:py-40">
